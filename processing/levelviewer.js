a8912d110d47dc6dd9334892a9a2a263edcc06c1
ringating <ringatingaling@gmail.com>
2019-05-15 16:37:53 -0700

    added levelviewer html and js

---
+++ processing/levelviewer.js
@@ -0,0 +1,42 @@
+var argStr = "";
+var canvasWidth = 0;
+var canvasHeight = 0;
+var cellLength = 0;
+
+function setup()
+{
+	createCanvas(windowWidth, windowHeight);
+	console.log(canvas.width + " " + canvas.height);
+	strokeWeight(2);
+	argStr = getURL();
+	argStr = argStr.slice(argStr.indexOf("?") + 1);
+	console.log("argStr = \"" + argStr + "\"")
+}
+
+function draw() 
+{
+	if(canvasWidth != canvas.width || canvasHeight != canvas.height)
+	{
+		canvasWidth = canvas.width;
+		canvasHeight = canvas.height;
+		
+		// ----- draw -----
+		
+		fill(0);
+		background(255);
+		
+		textAlign(CENTER, CENTER);
+		textSize(60);
+		text(argStr, canvasWidth/2, canvasHeight/2);
+		
+		//draw grid
+		// asdf
+		
+	}
+	
+}
+
+function windowResized()
+{
+	resizeCanvas(windowWidth, windowHeight);
+}
