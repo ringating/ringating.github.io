423091aebd8e86a3e03000bca46abee665b95f25
Chris <34548930+ringating@users.noreply.github.com>
2020-07-01 17:30:52 -0700

    Update scrolling-array.js
    something is off by one when calculating new values, oh well

--- processing/scrolling-array.js
+++ processing/scrolling-array.js
@@ -17,7 +17,7 @@ class ScrollingArray
         this.origin = new Coord(0,0);
         
         // initialize array
-        let count = 0;
+        // let count = 0;
         this.arr = new Array(arrSize);
         for(let i = 0; i < arrSize; ++i)
         {
@@ -26,7 +26,8 @@ class ScrollingArray
             {
                 //this.arr[i][j] = getRandomInt(10);
                 //this.arr[i][j] = (i + j) % arrSize;
-                this.arr[i][j] = count++;
+                //this.arr[i][j] = count++;
+                this.arr[i][j] = calcTile(i,j);
             }
         }
     }
@@ -39,7 +40,7 @@ class ScrollingArray
             retArr[i] = new Array(arrSize);
             for(let j = 0; j < arrSize; ++j)
             {
-                retArr[i][j] = this.arr[(this.origin.x + i) % arrSize][(this.origin.y + j) % arrSize];
+                retArr[i][j] = this.arr[wrap(this.origin.x + i)][wrap(this.origin.y + j)];
             }
         }
         
@@ -48,12 +49,54 @@ class ScrollingArray
     
     scrollHorizontal(dir) // 1 or -1
     {
-        this.origin.x = (this.origin.x + dir + arrSize) % arrSize;
+        this.origin.x += dir;
+        
+        if(dir === 1)
+        {
+            let col = wrap(this.origin.x - 1);
+            let row;
+            for(let j = 0; j < arrSize; ++j)
+            {
+                row = wrap(this.origin.y + j);
+                this.arr[col][row] = calcTile(this.origin.x + arrSize, this.origin.y + j);
+            }
+        }
+        else if(dir === -1)
+        {
+            let col = wrap(this.origin.x);
+            let row;
+            for(let j = 0; j < arrSize; ++j)
+            {
+                row = wrap(this.origin.y + j);
+                this.arr[col][row] = calcTile(this.origin.x, this.origin.y + j);
+            }
+        }
     }
     
     scrollVertical(dir) // 1 or -1
     {
-        this.origin.y = (this.origin.y + dir + arrSize) % arrSize;
+        this.origin.y += dir;
+        
+        if(dir === 1)
+        {
+            let row = wrap(this.origin.y - 1);
+            let col;
+            for(let i = 0; i < arrSize; ++i)
+            {
+                col = wrap(this.origin.x + i);
+                this.arr[col][row] = calcTile(this.origin.x + i, this.origin.y + arrSize);
+            }
+        }
+        else if(dir === -1)
+        {
+            let row = wrap(this.origin.y);
+            let col;
+            for(let i = 0; i < arrSize; ++i)
+            {
+                col = wrap(this.origin.x + i);
+                this.arr[col][row] = calcTile(this.origin.x + i, this.origin.y);
+            }
+        }
     }
 }
 
@@ -86,16 +129,16 @@ function draw()
     
     // draw actual array w/ origin lines
     translate(arrSize*tileSize, arrSize*tileSize);
-    for(let i = 0; i < arrSize; ++i)
-        for(let j = 0; j < arrSize; ++j)
+    for(let j = 0; j < arrSize; ++j)
+        for(let i = 0; i < arrSize; ++i)
             text(""+SA.arr[i][j], i*tileSize + tileSize/2, j*tileSize + tileSize/2);
     stroke(color(255, 0, 0));
-    line(Math.floor(tileSize * SA.origin.x)+0.5, 0.5, Math.floor(tileSize * SA.origin.x)+0.5, arrSize*tileSize+0.5);
-    line(0.5, Math.floor(tileSize * SA.origin.y)+0.5, arrSize*tileSize+0.5, Math.floor(tileSize * SA.origin.y)+0.5);
+    line(Math.floor(tileSize * wrap(SA.origin.x))+0.5, 0.5, Math.floor(tileSize * wrap(SA.origin.x))+0.5, arrSize*tileSize+0.5);
+    line(0.5, Math.floor(tileSize * wrap(SA.origin.y))+0.5, arrSize*tileSize+0.5, Math.floor(tileSize * wrap(SA.origin.y))+0.5);
 }
 
-function keyPressed() {
-    
+function keyPressed()
+{
     switch(keyCode)
     {
         case RIGHT_ARROW:
@@ -111,6 +154,23 @@ function keyPressed() {
             SA.scrollVertical(-1);
             break;
     }
+}
+
+function wrap(n)
+{
+    while(n < 0)
+        n += arrSize;
+    return (n) % arrSize;
+}
+
+function calcTile(i, j)
+{
+    let sign = 1;
+    
+    if((i < 0 || j < 0) && !(i < 0 && j < 0))
+        sign = -1;
+    
+    return Math.min(Math.abs(i), Math.abs(j)) * sign;
 }
 
 // // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
